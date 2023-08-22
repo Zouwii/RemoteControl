04f97e78383920b6313d48c295e7d7b228c12d60
zhr <zhrhuizou@126.com>
2023-08-22 21:45:57 +0800

    1.线程安全的队列模板实现

--- RemoteContorl/RemoteContorl/CZHRQueue.cpp
+++
@@ -1,2 +0,0 @@
-#include "pch.h"
-#include "CZHRQueue.h"
--- RemoteContorl/RemoteContorl/CZHRQueue.h
+++ RemoteContorl/RemoteContorl/CZHRQueue.h
@@ -1,44 +1,186 @@
 #pragma once
+#include "pch.h"
+#include <atomic>
+
 
 template <class T>
-
 class CZHRQueue
 {//线程安全队列 利用IOCP实现
 public:
-	CZHRQueue();
-	~CZHRQueue();
-	bool Push(const T& data);
-	bool Pop(T& data);
-	size_t Size();
-	void Clear();
-private:
-	static void threadEntry(void* arg);
-	void threadMain();
-private:
-	std::list <T>m_lstData;
-	HANDLE m_hCompletionPort;
-	HANDLE m_Thread;
-public:
-	typedef struct IocpParam {
-		int nOperator;//操作
-		T strData;//数据
-		_beginthread_proc_type cbFunc;//回调函数
-		HANDLE hEvent; //pop需要
-		IocpParam(int op, const char* sData, _beginthread_proc_type cb = NULL) {
-			nOperator = op;
-			strData = sData;
-		}
-		IocpParam() {
-			nOperator = -1;
-		}
-	}PPARAM; //PostParameter 用于投递信息结构体
-
 	enum {
+		EQNone,
 		EQPush,
 		EQPop,
 		EQSize,
 		EQClear,
 	};
-};
 
+	typedef struct IocpParam {
+		size_t nOperator;//操作
+		T Data;//数据
+		HANDLE hEvent; //pop需要
+		IocpParam(int op, const T& data, HANDLE hEve = NULL) {
+			nOperator = op;
+			Data = data;
+			hEvent = hEve;
+		}
+		IocpParam() {
+			nOperator = EQNone;
+		}
+	}PPARAM; //PostParameter 用于投递信息结构体
+
+	CZHRQueue() {
+		m_lock = false;
+		m_hCompletionPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, 1);
+		m_hThread = INVALID_HANDLE_VALUE;
+		if (m_hCompletionPort != NULL) {
+			m_hThread = (HANDLE)_beginthread(
+				&CZHRQueue::threadEntry, 0, m_hCompletionPort);
+		}
+	}
+	~CZHRQueue() {
+		if (m_lock)return;
+		m_lock = true;
+		HANDLE htemp = m_hCompletionPort;
+		PostQueuedCompletionStatus(m_hCompletionPort, 0, NULL, NULL);
+		WaitForSingleObject(m_hCompletionPort, INFINITE);
+		m_hCompletionPort = NULL;
+		CloseHandle(htemp);
+
+	}
+	bool PushBack(const T& data) {
+		if (m_lock) return false;
+		IocpParam* pParam = new IocpParam(EQPush, data);
+		bool ret = PostQueuedCompletionStatus(m_hCompletionPort, sizeof(PPARAM), (ULONG_PTR)pParam, NULL);
+		if (ret == false)delete pParam;
+		return ret;
+	}
+	bool PopFront(T& data) {
+		HANDLE hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+		IocpParam Param(EQPop, data, hEvent);
+		if (m_lock) {
+			if (hEvent) CloseHandle(hEvent);
+			return false;
+		}
+		bool ret = PostQueuedCompletionStatus(m_hCompletionPort, sizeof(PPARAM), (ULONG_PTR)&Param, NULL);
+		if (ret == false)
+		{
+			CloseHandle(hEvent);
+			return false;
+		}
+		ret = WaitForSingleObject(hEvent, INFINITE) == WAIT_OBJECT_0;
+		if (ret) {
+			data = Param.Data;
+		}
+		return ret;
+	}
+	size_t Size() {
+		HANDLE hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+		IocpParam Param(EQSize, T(), hEvent);
+		if (m_lock) {
+			if (hEvent) CloseHandle(hEvent);
+			return -1;
+		}
+		bool ret = PostQueuedCompletionStatus(m_hCompletionPort, sizeof(PPARAM), (ULONG_PTR)&Param, NULL);
+		if (ret == false)
+		{
+			CloseHandle(hEvent);
+			return -1;
+		}
+		ret = WaitForSingleObject(hEvent, INFINITE) == WAIT_OBJECT_0;
+		if (ret) {
+			return Param.nOperator;
+		}
+		return -1;
+	}
+	bool Clear() {
+		if (m_lock) return false;
+		IocpParam* pParam = new IocpParam(EQClear, T());
+		bool ret = PostQueuedCompletionStatus(m_hCompletionPort, sizeof(PPARAM), (ULONG_PTR)pParam, NULL);
+		if (ret == false)delete pParam;
+		return ret;
+	}
+private:
+	static void threadEntry(void* arg) {
+		CZHRQueue<T>* thiz = (CZHRQueue<T>*)arg;
+		thiz->threadMain();
+		_endthread();
+	}
+
+	void DealParam(PPARAM* pParam) {
+		switch (pParam->nOperator) {
+		case EQPush:
+		{
+			m_lstData.push_back(pParam->Data);
+			delete pParam;
+		}
+		break;
+		case EQPop:
+		{
+			if (m_lstData.size() > 0) {
+				pParam->Data = m_lstData.front();
+				m_lstData.pop_front();
+			}
+			if (pParam->hEvent != NULL) {
+				SetEvent(pParam->hEvent);
+			}
+		}
+		break;
+		case EQSize:
+		{
+			pParam->nOperator = m_lstData.size();
+			if (pParam->hEvent != NULL) {
+				SetEvent(pParam->hEvent);
+			}
+		}
+		break;
+		case EQClear:
+		{
+			m_lstData.clear();
+			delete pParam;
+		}
+		break;
+		default:
+			OutputDebugStringA("unknown operator!\r\n");
+			break;
+		}
+	}
+
+	void threadMain() {
+		DWORD dwTransferred = 0;
+		PPARAM* pParam = NULL;
+		ULONG_PTR CompletionKey = 0;
+		OVERLAPPED* pOverlapped = NULL;
+		while (GetQueuedCompletionStatus(
+			m_hCompletionPort,
+			&dwTransferred,
+			&CompletionKey,
+			&pOverlapped, INFINITE)) {
+			if ((dwTransferred == 0) || (CompletionKey == NULL)) {
+				printf("thread is prepared to exit!\r\n");
+				break;
+			}
+			pParam = (PPARAM*)CompletionKey;
+			DealParam(pParam);
+		}
+		while (GetQueuedCompletionStatus(
+			m_hCompletionPort,
+			&dwTransferred,
+			&CompletionKey,
+			&pOverlapped, 0)) {
+			if ((dwTransferred == 0) || (CompletionKey == NULL)) {
+				printf("thread is prepared to exit!\r\n");
+				continue;
+			}
+			pParam = (PPARAM*)CompletionKey;
+			DealParam(pParam);
+		}
+		CloseHandle(m_hCompletionPort);
+	}
+private:
+	std::list <T>m_lstData;
+	HANDLE m_hCompletionPort;
+	HANDLE m_hThread;
+	std::atomic<bool>m_lock;//队列正在析构
+};
 
